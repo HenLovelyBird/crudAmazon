d522e2179e3415de7a44e8a77bef2d5797477311
henlovelybird <voxylophone@gmail.com>
2019-11-13 15:50:00 +0100

    added base64

--- helper.js
+++ helper.js
@@ -1,3 +1,37 @@
-https://strive-school-testing-apis.herokuapp.com/api/product/
+const url = "https://striveschool.herokuapp.com/api/product";
 
-async (url)
+const username = "user16";
+const password = "c9WEUxMS294hN6fF";
+window.onload = async () => {
+  let response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Basic " + btoa(username + ":" + password)
+    }
+  });
+  console.log(response);
+};
+
+// const url = "https://strive-school-testing-apis.herokuapp.com/api/product/";
+
+// getProducts = async () => {
+//     const response = await fetch(url);
+//     return await response.json();
+// };
+
+// getProduct = async id => {
+//     const response = await fetch (url + id);
+//     return await response.json();
+// };
+
+// saveProduct = async event => {
+//     const response = await fetch (url, {
+//         method: "POST", 
+//         body:JSON.stringify(product),
+//         headers: new Headers({
+//             "Authorization": `Basic ${base64.encode(`${username}:${c9WEUxMS294hN6fF}`)}`
+//             // "Content-Type": "application/json"
+//         })
+//     });
+//     return response;
+// };
